1373a6f78a51d7980f3634bf337d768e2823ac1d
Bùi Quốc Trọng <quoctrong1x96@gmail.com>
2018-10-15 00:18:01 +0700

    2018-10-15 Commit project Mummy Maze Basic to Master (C sharp)

---
+++ C Sharp/MummyMazeBasic/ActorManager/ActorManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MummyMazeBasic
+{
+    public class ActorManager
+    {
+        private static ActorManager instance;
+
+        // List of actor
+        Actor mummyStupid;
+        Actor explorer;
+        Actor mummyIntelligent;
+        public int[][] local;
+        //Content manager
+        public ContentManager Content { get; set; }
+
+        /// <summary>
+        /// Output instance
+        /// </summary>
+        public static ActorManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ActorManager();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Initial actor
+        /// </summary>
+        public ActorManager()
+        {
+            explorer = new Explorer();
+            mummyStupid = new Mummy();
+            mummyIntelligent = new MummyIntelligent();
+
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            this.Content = new ContentManager(Content.ServiceProvider, "Content");
+            explorer.LoadContent();
+            mummyIntelligent.LoadContent();
+            mummyStupid.LoadContent();
+        }
+
+        public void UploadConten()
+        {
+            explorer.UploadConten();
+            mummyIntelligent.UploadConten();
+            mummyStupid.UploadConten();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            explorer.Update(gameTime);
+            mummyIntelligent.Update(gameTime);
+            mummyStupid.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            explorer.Draw(spriteBatch);
+            mummyIntelligent.Draw(spriteBatch);
+            mummyStupid.Draw(spriteBatch);
+        }
+    }
+}
+
---
+++ C Sharp/MummyMazeBasic/Options/OptionsManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MummyMazeBasic
+{
+    public class OptionsManager
+    {
+        private static OptionsManager instance;
+
+        Options gameover;
+        Options backOptios;
+        Options gameOptions;
+        Options mouse;
+        Options buttonOKCANCEL;
+
+        public ContentManager Content { get; set; }
+
+        public static OptionsManager Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new OptionsManager();
+                }
+                return instance;
+            }
+        }
+
+        public OptionsManager()
+        {
+            gameover = new OverGame();
+            backOptios = new BackOptions();
+            gameOptions = new GameOptions();
+            mouse = new MousePointer();
+            buttonOKCANCEL = new ButtonOKCANCEL();
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            this.Content = new ContentManager(Content.ServiceProvider, "Content");
+            gameover.LoadContent();
+            backOptios.LoadContent();
+            gameOptions.LoadContent();
+            mouse.LoadContent();
+            buttonOKCANCEL.LoadContent();
+        }
+
+        public void UploadConten()
+        {
+            gameover.UploadConten();
+            backOptios.UploadConten();
+            gameOptions.UploadConten();
+            mouse.UploadConten();
+            buttonOKCANCEL.UploadConten();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            gameover.Update(gameTime);
+            backOptios.Update(gameTime);
+            gameOptions.Update(gameTime);
+            mouse.Update(gameTime);
+            buttonOKCANCEL.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (GameMapManager.Instance.isGameOver)
+            {
+                backOptios.Draw(spriteBatch);
+                gameover.Draw(spriteBatch);
+            }
+            if (!GameMapManager.Instance.isButtonOption)
+            {
+                backOptios.Draw(spriteBatch);
+                gameOptions.Draw(spriteBatch);
+                buttonOKCANCEL.Draw(spriteBatch);
+            }
+            mouse.Draw(spriteBatch);
+        }
+    }
+}
+
